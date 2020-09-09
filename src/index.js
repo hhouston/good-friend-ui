cfbe289fd8a3cbdec848654e51bb54b9a32088a1
Hunter Houston <hmhouston7@gmail.com>
2020-09-08 22:26:47 -0500

    Add cors same origin to client

--- src/index.js
+++ src/index.js
@@ -42,6 +42,7 @@ const client = new ApolloClient({
   // uri: "https://good-friend-1269800380.us-east-1.elb.amazonaws.com/graphql",
   // uri: "https://3.222.145.116/graphql",
   uri: "https://api.thankyougift.io/graphql",
+  credentials: 'same-origin',
   // uri: "https://54.80.191.226/graphql",
   cache: new InMemoryCache(),
 });
