2837b0bfba9282edff96693ae9a87f5e94cd596d
Hunter Houston <hmhouston7@gmail.com>
2020-09-22 16:10:54 -0500

    Uncomment create upload link

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Root from "./Root";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-// import { createUploadLink } from "apollo-upload-client";
+import { createUploadLink } from "apollo-upload-client";
 
 import { loadState, saveState } from './localStorage';
 
