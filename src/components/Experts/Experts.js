08a12448e65ac20cf73e6e77202c6275b2617899
Chandrika Prasad Shah <chandrika.s@chapter247.com>
2022-02-08 20:09:34 +0530

    Added arrows on left and right side

--- src/components/Experts/Experts.js
+++ src/components/Experts/Experts.js
@@ -150,14 +150,14 @@ const settings = {
             breakpoint: 1024,
             settings: {
                 slidesToShow: 2,
-                slidesToShow: 1,
+                slidesToScroll: 2,
             }
         },
         {
             breakpoint: 600,
             settings: {
                 slidesToShow: 1,
-                slidesToShow: 1,
+                slidesToScroll: 1,
             }
         }
     ]
